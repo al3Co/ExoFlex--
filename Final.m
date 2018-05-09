02886ee4770e4e44ea980db62abc25ae74a5368f
AldoContreGo <aldocontrego@gmail.com>
2018-05-09 18:05:04 +0200

    improvements
    create CSV file
    create Workspace .mat

--- Final.m
+++ Final.m
@@ -7,18 +7,22 @@ prompt = ('Enter sensor data file name (XLS file): ');
 sensorFile = input(prompt,'s');
 prompt = ('Enter optitrack data file name (XLS file): ');
 optiTrackXLS = input(prompt,'s');
-prompt = ('Kind of Movement made: ');
+prompt = ('Kind of Movement made (COMBO, CRUZEXT, CRUZINT, ...): ');
 typeOfMov = input(prompt,'s');
 
 %% calculations
 tic
+disp('Reading files...')
 % function to extract OptiTrack data and filling void spaces
 [tableOpti] = OptiCoord(optiTrackXLS);
 % function to get sensors table and Match time with OptiTrack Data
 [tableSensorsData, tableMatched] = QuaternTimeCheck(tableOpti, sensorFile);
-[tableMatched] = addType(tableMatched, typeOfMov);
-
-%% create CSV file
+% add type of movement
+[allDataTable] = addType(tableSensorsData, tableMatched, typeOfMov);
 toc
-writetable(tableMatched,strcat(typeOfMov,'.csv'),'Delimiter',',','QuoteStrings',true);
-disp('done')
+
+%% create CSV file and save Workspace
+fileName = sensorFile(1:end-4);
+writetable(allDataTable,strcat(fileName,'.csv'),'Delimiter',',','QuoteStrings',true);
+save(fileName)
+disp('Done')
