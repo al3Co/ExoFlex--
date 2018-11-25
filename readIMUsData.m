f4f7026a9806396d368204c2a900a883bd2c3297
Aldo Contreras <aldocontrego@gmail.com>
2018-11-25 20:25:40 +0100

    expand changed to 10
    expand changed to 10

--- readIMUsData.m
+++ readIMUsData.m
@@ -5,38 +5,41 @@ clc
 
 Ttotal = [];
 T3 = [];
+expand = 10;
+plotOpt = false;
 c = 1;
-expand = 20;
 % import data direcotry
 dir_to_search = 'C:\Users\disam\OneDrive - Universidad Politécnica de Madrid\ExoFlex\Publications\Access\Tests\dataAcquired\test2\IMUs';
 txtpattern = fullfile(dir_to_search, '*.txt');
 dinfo = dir(txtpattern);
-
+tic
 for K = 1 : length(dinfo)
     filename = fullfile(dir_to_search, dinfo(K).name);  %just the name
     disp(filename)
+    
     T = readtable(filename);
-    DateVector = [interpolFunc(datevec(T.Var1(1)),expand,1), interpolFunc(T.Var2(1),expand,2)];
+    DateVector = interpolSecsFunc(interpolFunc(datevec(T.Var1),expand,1));
     imusQuat = interpolFunc(table2array(T(:,14:17)),expand,3);
+    
     imuData = [DateVector, imusQuat];
+    
     if c<=3
         T3 = [T3, imusQuat];
     end
-
-%     plot(imusQuat)
-%     title(filename)
-%     pause(4)
-    
+    if plotOpt
+        plot(imusQuat)
+        title(filename)
+        pause(3)
+    end
     if c == 3
         Ttotal = [Ttotal; T3];
-%         plot(T3)
-%         pause(1)
         T3=[];
         c = 0;
     end
     c = c+1;
 end
-% plot(Ttotal)
+toc
+plot(Ttotal)
 
 % read
 % T(:,1:2);     % hour
