95b8719bc2518dcf0f26b619b31ade9573f4b86b
AC <aldocontrego@gmail.com>
2018-11-26 19:28:03 +0100

    Change file name
    Change file name to display

--- readFlexSens.m
+++ readFlexSens.m
@@ -4,20 +4,22 @@ clc
 
 Ttotal = [];
 % import data direcotry
-dir_to_search = 'C:\Users\disam\OneDrive - Universidad Politécnica de Madrid\ExoFlex\Publications\Access\Tests\dataAcquired\test2\FlexSens';
+dir_to_search = 'C:\Users\disam\Documents\OneDrive_AldoContreras\OneDrive - Universidad Politécnica de Madrid\ExoFlex\Publications\Access\Tests\dataAcquired\test2\FlexSens';
 txtpattern = fullfile(dir_to_search, '*.mat');
 dinfo = dir(txtpattern);
 
 % read each file
 for K = 1 : length(dinfo)
     filename = fullfile(dir_to_search, dinfo(K).name);  %just the name
-    disp(filename)
+    name = erase(extractAfter(filename,'1811'),'.mat');
+    disp(name)
     T = struct2cell(load(filename));
     T = T{1};
     sens = [T.S1, T.S2, T.S3, T.S4, T.S5, T.S6, T.S7, T.S8, T.S9, T.S10];
     Ttotal = [Ttotal; sens];
     plot(sens)
-    pause(2)
+    disp(K)
+    pause(1)
 end
 
 plot(Ttotal)
