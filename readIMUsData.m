e6993d56138eece51cbc16b674cc9b5cc28e79ae
AC <aldocontrego@gmail.com>
2018-11-26 12:17:10 +0100

    Major Update, save Workspace 3 sens
    Major Update, save Workspace 3 sens

--- readIMUsData.m
+++ readIMUsData.m
@@ -1,46 +1,56 @@
-% read txt with IMUs data
+% read txt on directory and create workspace with 3 IMUs data
 clear
 close
 clc
 
-Ttotal = [];
-T3 = [];
 expand = 10;
-plotOpt = false;
 c = 1;
 % import data direcotry
-dir_to_search = 'C:\Users\disam\OneDrive - Universidad Politécnica de Madrid\ExoFlex\Publications\Access\Tests\dataAcquired\test2\IMUs';
+dir_to_search = 'C:\Users\disam\Documents\OneDrive_AldoContreras\OneDrive - Universidad Politécnica de Madrid\ExoFlex\Publications\Access\Tests\dataAcquired\test2\IMUs';
 txtpattern = fullfile(dir_to_search, '*.txt');
 dinfo = dir(txtpattern);
 tic
 for K = 1 : length(dinfo)
+    % for each file
     filename = fullfile(dir_to_search, dinfo(K).name);  %just the name
     disp(filename)
-    
     T = readtable(filename);
-    DateVector = interpolSecsFunc(interpolFunc(datevec(T.Var1),expand,1));
-    imusQuat = interpolFunc(table2array(T(:,14:17)),expand,3);
-    
-    imuData = [DateVector, imusQuat];
-    
-    if c<=3
-        T3 = [T3, imusQuat];
+    % read first IMU
+    if c == 1
+        DateVector = interpolSecsFunc(interpolFunc(datevec(T.Var1),expand,1));
+        HH = DateVector(:,1);
+        MM = DateVector(:,2);
+        SS = DateVector(:,3);
+        MS = DateVector(:,4);
+        t_result = table(HH,MM,SS,MS);
+        data = num2cell(interpolFunc(table2array(T(:,14:20)),expand,3));
+        t_result(:,5:11) = data;
+        varNames = {'HH','MM','SS','MS','QB1','QB2','QB3','QB4','EuB1','EuB2','EuB3'};
+        t_result.Properties.VariableNames = varNames;
     end
-    if plotOpt
-        plot(imusQuat)
-        title(filename)
-        pause(3)
+    % read second IMU
+    if c == 2
+        data = num2cell(interpolFunc(table2array(T(:,14:20)),expand,3));
+        t_result(:,12:18) = data;
+        varNames = {'HH','MM','SS','MS','QB1','QB2','QB3','QB4','EuB1','EuB2','EuB3'...
+            'QA1','QA2','QA3','QA4','EuA1','EuA2','EuA3'};
+        t_result.Properties.VariableNames = varNames;
     end
+    % read third IMU
     if c == 3
-        Ttotal = [Ttotal; T3];
-        T3=[];
+        data = num2cell(interpolFunc(table2array(T(:,14:20)),expand,3));
+        t_result(:,19:25) = data;
+        varNames = {'HH','MM','SS','MS','QB1','QB2','QB3','QB4','EuB1','EuB2','EuB3'...
+            'QA1','QA2','QA3','QA4','EuA1','EuA2','EuA3'...
+            'QArm1','QArm2','QArm3','QArm4','EuArm1','EuArm2','EuArm3'};
+        t_result.Properties.VariableNames = varNames;
+        nameSave = strcat(erase(extractAfter(filename,'11-'),'.txt'),'.mat');
+        save(nameSave,'t_result')
         c = 0;
     end
     c = c+1;
-    % save(filename,variables)
 end
 toc
-plot(Ttotal)
 
 % read
 % T(:,1:2);     % hour
